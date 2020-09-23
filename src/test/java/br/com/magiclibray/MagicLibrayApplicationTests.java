3c22ef06fe019016108df003ef88106f7d12974e
João Pedro <joao.pedro.fehc@hotmail.com>
2020-09-23 16:02:19 -0300

    Initializing with a basic project.

---
+++ src/test/java/br/com/magiclibray/MagicLibrayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.magiclibray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicLibrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
