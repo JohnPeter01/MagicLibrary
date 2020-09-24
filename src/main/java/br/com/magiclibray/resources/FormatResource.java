eba773543b01a657d966b495afb7c9b0d479ad82
João Pedro <joao.pedro.fehc@gmail.com>
2020-09-24 19:06:37 -0300

    Creating Format Domain class and testing endpoint.

--- src/main/java/br/com/magiclibray/resources/FormatResource.java
+++ src/main/java/br/com/magiclibray/resources/FormatResource.java
@@ -1,15 +1,33 @@
 package br.com.magiclibray.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.magiclibray.domain.Format;
+
 @RestController
 @RequestMapping(value = "/format")
 public class FormatResource {
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "Format Working";
+	public List<Format> list() {
+		
+		Format standard = new Format();
+		standard.setId(1);
+		standard.setName("Standard");
+		Format modern = new Format();
+		modern.setId(1);
+		modern.setName("Modern");
+		
+		List<Format> formats = new ArrayList<Format>();
+		formats.add(standard);
+		formats.add(modern);
+		
+		return formats;
 	}
 
 }
